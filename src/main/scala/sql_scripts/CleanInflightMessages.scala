5d2caf8c730761e133245ee74d4475859821aa9d
Mechy <rit6569@gmail.com>
2023-03-16 14:16:11 +0530

    Add Housekeeper pt2

--- src/main/scala/sql_scripts/CleanInflightMessages.scala
+++ src/main/scala/sql_scripts/CleanInflightMessages.scala
@@ -9,7 +9,7 @@ import scala.concurrent.Future
 import scala.concurrent.ExecutionContext.Implicits.global
 
 object CleanInflightMessages {
-    def CleanInflightMessages(): Future[String] = {
+    def CleanInflightMessages(): Unit = {
 
         val db = getDB()
         val cur_ts = None;
